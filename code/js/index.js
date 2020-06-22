bef76b0ace8c02bbf2115891c114e557a61965bd
hyemin-yoo94 <m01024322773@gmail.com>
2020-06-22 18:08:20 +0900

    first commit

---
+++ code/js/index.js
@@ -0,0 +1,30 @@
+console.clear();
+
+$(function() {
+    $.scrollify({
+        section:".page",
+        scrollbars:false,
+        before:function(i,panels) {
+            var ref = panels[i].attr("data-section-name");
+            $(".pagination .active").removeClass("active");
+            $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+        },
+        afterRender:function() {
+            var pagination = "<ul class=\"pagination\">";
+            var activeClass = "";
+            $(".page").each(function(i) {
+                activeClass = "";
+                if ( i === 0 ) {
+                    activeClass = "active";
+                }
+                pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+            });
+
+            pagination += "</ul>";
+
+            $(".page-indicator-box").append(pagination);
+
+            $(".pagination a").on("click", $.scrollify.move);
+        }
+    });
+});
